f8d7b44973a65e6e8027ceab94fbecc628f202df
Dallas Gale <dallas@mylocalbroker.com.au>
2016-05-11 12:03:06 +1000

    site update

--- app/scripts/controllers/eyes.js
+++ app/scripts/controllers/eyes.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /**
  * @ngdoc function
  * @name yeomanApp.controller:EyesCtrl
@@ -9,146 +8,269 @@
  */
 angular.module('yeomanApp')
   .controller('EyesCtrl', function ($scope) {
-   $scope.galleries = [
+    $scope.galleries = [
       {
-      image: 'images/gallery/theintheout-1.jpg',
-      imgAlt: 'The In The Out'
-      },
-      // {
-      // image: 'images/gallery/theintheout-2.jpg',
-      // imgAlt: 'The In The Out'
-      // },
-      {
-      image: 'images/gallery/theintheout-3.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-old-bar-barry-takes-photos-1.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://barrytakesphotos.com/',
+        copyright: 'Melbourne Australia - 2016 Photography by Barry C Douglas'
       },
       {
-      image: 'images/gallery/theintheout-4.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-old-bar-barry-takes-photos-2.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://barrytakesphotos.com/',
+        copyright: 'Melbourne Australia - 2016 Photography by Barry C Douglas'
       },
       {
-      image: 'images/gallery/theintheout-5.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-old-bar-barry-takes-photos-3.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://barrytakesphotos.com/',
+        copyright: 'Melbourne Australia - 2016 Photography by Barry C Douglas'
       },
       {
-      image: 'images/gallery/theintheout-6.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-old-bar-barry-takes-photos-5.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://barrytakesphotos.com/',
+        copyright: 'Melbourne Australia - 2016 Photography by Barry C Douglas'
+      },
+//      {
+//        image: 'http://www.theintheout.com/images/gallery/theintheout-old-bar-barry-takes-photos-6.jpg',
+//        imgAlt: 'The In The Out',
+//        imgLink: 'http://barrytakesphotos.com/',
+//        copyright: 'Melbourne Australia - 2016 Photography by Barry C Douglas'
+//      },
+      {
+        image: 'http://www.theintheout.com/images/gallery/theintheout-old-bar-barry-takes-photos-7.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://barrytakesphotos.com/',
+        copyright: 'Melbourne Australia - 2016 Photography by Barry C Douglas'
       },
       {
-      image: 'images/gallery/theintheout-7.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-old-bar-barry-takes-photos-8.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://barrytakesphotos.com/',
+        copyright: 'Melbourne Australia - 2016 Photography by Barry C Douglas'
       },
       {
-      image: 'images/gallery/theintheout-8.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-old-bar-barry-takes-photos-10.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://barrytakesphotos.com/',
+        copyright: 'Melbourne Australia - 2016 Photography by Barry C Douglas'
       },
       {
-      image: 'images/gallery/theintheout-9.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-old-bar-barry-takes-photos-11.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://barrytakesphotos.com/',
+        copyright: 'Melbourne Australia - 2016 Photography by Barry C Douglas'
+      },
+//      {
+//        image: 'http://www.theintheout.com/images/gallery/theintheout-old-bar-barry-takes-photos-12.jpg',
+//        imgAlt: 'The In The Out',
+//        imgLink: 'http://barrytakesphotos.com/',
+//        copyright: 'Melbourne Australia - 2016 Photography by Barry C Douglas'
+//      },
+      {
+        image: 'http://www.theintheout.com/images/gallery/theintheout-old-bar-barry-takes-photos-13.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://barrytakesphotos.com/',
+        copyright: 'Melbourne Australia - 2016 Photography by Barry C Douglas'
+      },
+//      {
+//        image: 'http://www.theintheout.com/images/gallery/theintheout-old-bar-barry-takes-photos-14.jpg',
+//        imgAlt: 'The In The Out',
+//        imgLink: 'http://barrytakesphotos.com/',
+//        copyright: 'Melbourne Australia - 2016 Photography by Barry C Douglas'
+//      },
+      {
+        image: 'http://www.theintheout.com/images/gallery/theintheout-1.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://ilanarose.zenfolio.com/',
+        copyright: 'Melbourne, Australia - 2016 Photography by Ilana Rose'
       },
       {
-      image: 'images/gallery/theintheout-10.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-3.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://ilanarose.zenfolio.com/',
+        copyright: 'Melbourne, Australia - 2016 Photography by Ilana Rose'
       },
       {
-      image: 'images/gallery/theintheout-11.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-4.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://ilanarose.zenfolio.com/',
+        copyright: 'Melbourne, Australia - 2016 Photography by Ilana Rose'
       },
       {
-      image: 'images/gallery/theintheout-12.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-5.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://ilanarose.zenfolio.com/',
+        copyright: 'Melbourne, Australia - 2016 Photography by Ilana Rose'
       },
       {
-      image: 'images/gallery/theintheout-13.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-6.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://ilanarose.zenfolio.com/',
+        copyright: 'Melbourne, Australia - 2016 Photography by Ilana Rose'
       },
       {
-      image: 'images/gallery/theintheout-14.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-7.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://ilanarose.zenfolio.com/',
+        copyright: 'Melbourne, Australia - 2016 Photography by Ilana Rose'
       },
       {
-      image: 'images/gallery/theintheout-15.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-8.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://ilanarose.zenfolio.com/',
+        copyright: 'Melbourne, Australia - 2016 Photography by Ilana Rose'
       },
       {
-      image: 'images/gallery/theintheout-16.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-9.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://ilanarose.zenfolio.com/',
+        copyright: 'Melbourne, Australia - 2016 Photography by Ilana Rose'
       },
       {
-      image: 'images/gallery/theintheout-17.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-10.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://ilanarose.zenfolio.com/',
+        copyright: 'Melbourne, Australia - 2016 Photography by Ilana Rose'
       },
       {
-      image: 'images/gallery/theintheout-18.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-11.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://ilanarose.zenfolio.com/',
+        copyright: 'Melbourne, Australia - 2016 Photography by Ilana Rose'
       },
       {
-      image: 'images/gallery/theintheout-19.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-12.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://ilanarose.zenfolio.com/',
+        copyright: 'Melbourne, Australia - 2016 Photography by Ilana Rose'
       },
       {
-      image: 'images/gallery/theintheout-20.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-13.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://ilanarose.zenfolio.com/',
+        copyright: 'Melbourne, Australia - 2016 Photography by Ilana Rose'
       },
       {
-      image: 'images/gallery/theintheout-21.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-14.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://ilanarose.zenfolio.com/',
+        copyright: 'Melbourne, Australia - 2016 Photography by Ilana Rose'
       },
       {
-      image: 'images/gallery/theintheout-22.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-15.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://ilanarose.zenfolio.com/',
+        copyright: 'Melbourne, Australia - 2016 Photography by Ilana Rose'
       },
       {
-      image: 'images/gallery/theintheout-23.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-16.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://ilanarose.zenfolio.com/',
+        copyright: 'Melbourne, Australia - 2016 Photography by Ilana Rose'
       },
       {
-      image: 'images/gallery/theintheout-24.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-17.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://ilanarose.zenfolio.com/',
+        copyright: 'Melbourne, Australia - 2016 Photography by Ilana Rose'
       },
       {
-      image: 'images/gallery/theintheout-25.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-18.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://ilanarose.zenfolio.com/',
+        copyright: 'Melbourne, Australia - 2016 Photography by Ilana Rose'
       },
       {
-      image: 'images/gallery/theintheout-26.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-19.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://ilanarose.zenfolio.com/',
+        copyright: 'Melbourne, Australia - 2016 Photography by Ilana Rose'
       },
       {
-      image: 'images/gallery/theintheout-27.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-20.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://ilanarose.zenfolio.com/',
+        copyright: 'Melbourne, Australia - 2016 Photography by Ilana Rose'
       },
       {
-      image: 'images/gallery/theintheout-28.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-21.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://ilanarose.zenfolio.com/',
+        copyright: 'Melbourne, Australia - 2016 Photography by Ilana Rose'
       },
       {
-      image: 'images/gallery/theintheout-29.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-22.jpg',
+        imgAlt: 'The In The Out',
+        imgLink: 'http://ilanarose.zenfolio.com/',
+        copyright: 'Melbourne, Australia - 2016 Photography by Ilana Rose'
       },
       {
-      image: 'images/gallery/theintheout-30.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-23.jpg',
+        imgAlt: 'The In The Out',
+        copyright: 'Melbourne, Australia - 2013'
       },
       {
-      image: 'images/gallery/theintheout-31.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-24.jpg',
+        imgAlt: 'The In The Out',
+        copyright: 'Melbourne, Australia - 2013'
       },
       {
-      image: 'images/gallery/theintheout-32.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-25.jpg',
+        imgAlt: 'The In The Out',
+        copyright: 'Melbourne, Australia - 2013'
       },
       {
-      image: 'images/gallery/theintheout-33.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-26.jpg',
+        imgAlt: 'The In The Out',
+        copyright: 'Melbourne, Australia - 2013'
       },
       {
-      image: 'images/gallery/theintheout-34.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-27.jpg',
+        imgAlt: 'The In The Out',
+        copyright: 'Melbourne, Australia - 2013'
       },
       {
-      image: 'images/gallery/theintheout-35.jpg',
-      imgAlt: 'The In The Out'
+        image: 'http://www.theintheout.com/images/gallery/theintheout-28.jpg',
+        imgAlt: 'The In The Out',
+        copyright: 'Melbourne, Australia - 2013'
       },
+      {
+        image: 'http://www.theintheout.com/images/gallery/theintheout-29.jpg',
+        imgAlt: 'The In The Out',
+        copyright: 'Melbourne, Australia - 2013'
+      },
+      {
+        image: 'http://www.theintheout.com/images/gallery/theintheout-30.jpg',
+        imgAlt: 'The In The Out',
+        copyright: 'Melbourne, Australia - 2013'
+      },
+      {
+        image: 'http://www.theintheout.com/images/gallery/theintheout-31.jpg',
+        imgAlt: 'The In The Out',
+        copyright: 'Melbourne, Australia - 2013'
+      },
+      {
+        image: 'http://www.theintheout.com/images/gallery/theintheout-32.jpg',
+        imgAlt: 'The In The Out',
+        copyright: 'Melbourne, Australia - 2013'
+      },
+      {
+        image: 'http://www.theintheout.com/images/gallery/theintheout-33.jpg',
+        imgAlt: 'The In The Out',
+        copyright: 'Melbourne, Australia - 2013'
+      },
+      {
+        image: 'http://www.theintheout.com/images/gallery/theintheout-34.jpg',
+        imgAlt: 'The In The Out',
+        copyright: 'Melbourne, Australia - 2013'
+      },
+      {
+        image: 'http://www.theintheout.com/images/gallery/theintheout-35.jpg',
+        imgAlt: 'The In The Out',
+        copyright: 'Melbourne, Australia - 2013'
+      }
     ];
   });
--- app/scripts/controllers/live.js
+++ app/scripts/controllers/live.js
@@ -11,77 +11,56 @@ angular.module('yeomanApp')
   .controller('LiveCtrl', function ($scope) {
     
     $scope.gigs = [
+//      {
+//        venue: 'Tago Mago',
+//        location: 'Thornbury, Vic AU',
+//        date: '22.04.2016',
+//        imgUrl: '',
+//        imgAlt: '',
+//        linkUrl: 'https://www.facebook.com/ClubTagoMago/',
+//        linkTitle: 'Tago Mago',
+//        content: '11pm // supports: THE NIGHT COLLECTORS // $8'
+//      },
+//      {
+//        venue: 'The Old Bar',
+//        location: 'Fitzroy, Vic AU',
+//        date: '01.05.2016',
+//        imgUrl: '',
+//        imgAlt: '',
+//        linkUrl: 'http://www.theoldbar.com.au/',
+//        linkTitle: 'The Old Bar',
+//        content: '9pm // supporting: KIM SALMON // $TBC'
+//      },
       {
-      venue: 'Gin Lane',
-      location: 'Belgrave, Vic AU',
-      date: '30.01.2016',
-      imgUrl: '../images/posters/Jan-30.jpg',
-      imgAlt: 'January 30 - Gin Lane (Belgrave VIC)',
-      linkUrl: 'http://www.google.com',
-      linkTitle: 'Gin Lane',
-      content: 'We play the Old Bar to bring in xmas with special guests The Night Collectors. '
+        venue: 'Penny Black',
+        location: 'Brunswick, Vic AU',
+        date: '21.05.2016',
+        imgUrl: '',
+        imgAlt: '',
+        linkUrl: 'https://www.facebook.com/events/1609528096036173/',
+        linkTitle: 'FB event',
+        content: '9pm // supports: SWAMP + NOTI // FREE'
       },
       {
-      venue: 'The Retreat',
-      location: 'Belgrave, Vic AU',
-      date: '26.01.2016',
-      imgUrl: '',
-      imgAlt: '',
-      linkUrl: 'http://www.google.com',
-      linkTitle: 'Retreat Hotel',
-      content: 'We play the Old Bar to bring in xmas with special guests The Night Collectors. '
+        venue: 'The Old Bar (Duo) w/KIM SALMON',
+        location: 'Fitzroy, Vic AU',
+        date: '22.05.2016',
+        imgUrl: '',
+        imgAlt: '',
+        linkUrl: 'https://www.facebook.com/events/1771875139709520/',
+        linkTitle: 'FB event',
+        content: 'Dallas Gale & Will Hindmarsh only! // 8:40pm // supporting: KIM SALMON // $TBC'
       },
       {
-      venue: 'The Penny Black',
-      location: 'Belgrave, Vic AU',
-      date: '23.01.2016',
-      imgUrl: '',
-      imgAlt: '',
-      linkUrl: 'http://www.google.com',
-      linkTitle: 'Penny Black',
-      content: 'We play the Old Bar to bring in xmas with special guests The Night Collectors. '
+        venue: 'Lyrebird Lounge',
+        location: 'Glen Eira, Vic AU',
+        date: '27.05.2016',
+        imgUrl: '',
+        imgAlt: '',
+        linkUrl: 'https://www.facebook.com/LyrebirdLounge/',
+        linkTitle: 'Lyrebird Lounge',
+        content: '10pm // supports: // FREE'
       }
-    //   ,
-    //   {
-    //   venue: 'The Old Bar',
-    //   location: 'Fitzroy, Vic AU',
-    //   date: '20.12.2015',
-    //   imgUrl: '../images/posters/Dec-20.jpg',
-    //   imgAlt: 'December 20 - The Old Bar (Fitzroy VIC)',
-    //   linkUrl: 'http://www.google.com',
-    //   linkTitle: 'The Old Bar',
-    //   content: 'Australia Day at the Retreat featuring... '
-    //   },
-    //   {
-    //   venue: 'Gin Lane',
-    //   location: 'Belgrave, Vic AU',
-    //   date: '5.11.2015',
-    //   imgUrl: '../images/posters/Nov-5.jpg',
-    //   imgAlt: 'January 30 - Gin Lane (Belgrave VIC)',
-    //   linkUrl: 'http://www.google.com',
-    //   linkTitle: 'Google',
-    //   content: 'We play the Old Bar to bring in xmas with special guests The Night Collectors. '
-    //   },
-    //   {
-    //   venue: 'Toff In Town',
-    //   location: 'Melbourne, Vic AU',
-    //   date: '7.02.2015',
-    //   imgUrl: '../images/posters/Feb-7.jpg',
-    //   imgAlt: 'Feb 7 - Toff In Town (Melbourne VIC)',
-    //   linkUrl: 'http://www.google.com',
-    //   linkTitle: 'Google',
-    //   content: 'We play the Old Bar to bring in xmas with special guests The Night Collectors. '
-    //   },
-    //   {
-    //   venue: 'Boney',
-    //   location: 'Melbourne, Vic AU',
-    //   date: '15.02.2014',
-    //   imgUrl: '../images/posters/Jan-15-2014.jpg',
-    //   imgAlt: 'Feb 7 - Toff In Town (Melbourne VIC)',
-    //   linkUrl: 'http://www.google.com',
-    //   linkTitle: 'Google',
-    //   content: 'We play the Old Bar to bring in xmas with special guests The Night Collectors. '
-    //   }
     ];
     
     
